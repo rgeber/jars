ab8b3166546ef4eaaab4293e5d0e0a3676995d17
Roman Geber <dlz9zxdkjptu@romangeber.com>
2024-09-08 12:01:09 +0200

    Surrealdb module refactored to watch for a change in login status

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,48 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      <button @click="logout('oidc')">Logout</button>
+    </p>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import {type Jar, jarSchema} from "~/types/jar"
 import {RecordId} from "surrealdb.js";
 import {type User, userSchema} from "~/types/user";
-const { $surreal } = useNuxtApp();
+// const { $surreal } = useNuxtApp();
+//
+// const {logout} = useOidcAuth()
+//
+// const userData: User = {
+//   id: new RecordId('users', 'hans'),
+//   email: 'email@me.tld',
+//   username: 'Hans',
+//   creationDate: new Date,
+//   kind: "User"
+// }
+//
+// const user = userSchema.safeParse(userData);
+//
+// if (user.success) {
+//   const jarData: Jar = {
+//     id: new RecordId('test', 'nix'),
+//     title: 'xx',
+//     creationDate: new Date,
+//     kind: 'Jar',
+//     owner: user.data.id
+//   }
+//   const jar = jarSchema.safeParse(jarData)
+//   console.log(jar)
+// }
 
-const userData: User = {
-  id: new RecordId('users', 'hans'),
-  email: 'email@me.tld',
-  username: 'Hans',
-  creationDate: new Date,
-  kind: "User"
-}
-
-const user = userSchema.safeParse(userData);
-
-if (user.success) {
-  const jarData: Jar = {
-    id: new RecordId('test', 'nix'),
-    title: 'xx',
-    creationDate: new Date,
-    kind: 'Jar',
-    owner: user.data.id
-  }
-  const jar = jarSchema.safeParse(jarData)
-  console.log(jar)
-}
 
 
-
-onBeforeMount(async () => {
-  const ns = await $surreal.query("INFO FOR NS;")
-  console.log(ns)
-})
+// onBeforeMount(async () => {
+//   const ns = await $surreal.query("INFO FOR NS;")
+//   console.log(ns)
+// })
 
 
 
--- plugins/surrealdb.ts
+++ plugins/surrealdb.ts
@@ -1,35 +1,47 @@
 import Surreal from "surrealdb.js";
+import {watch} from 'vue';
 
 export default defineNuxtPlugin((nuxtApp) => {
 
     console.debug('Initializing SurrealDB plugin.');
 
     const config = useRuntimeConfig().public.surrealdb
+    const {loggedIn, user} = useOidcAuth()
 
     const surreal = new Surreal()
 
-    const connDB = async() => {
-            try {
-                await surreal.connect(config.url)
-                await surreal.use({
-                    namespace: config.ns,
-                    database: config.db
-                })
-                // TODO: Remove in favor of token auth
-                await surreal.signin({
-                    username: 'root',
-                    password: 'root'
-                })
-            } catch (e) {
-                console.error("Failed to connect to SurrealDB", e)
+    const connDB = async () => {
+        try {
+            if (typeof user.value.accessToken !== 'string') {
+                console.error('SurrealDB connection failed. User access token is undefined.')
+                return;
             }
+            console.debug('Establishing SurrealDB connection', config.url)
+            await surreal.connect(config.url)
+
+            console.debug(`Using SurrealDB namespace '${config.ns}' with database '${config.db}'`)
+            await surreal.use({
+                namespace: config.ns,
+                database: config.db
+            })
+
+            await surreal.authenticate(user.value.accessToken!)
+            console.debug("SurrealDB connected successfully")
+
+        } catch (e) {
+            console.error("Failed to connect to SurrealDB", e)
         }
+    }
 
     if (import.meta.client) {
-        connDB()
-            .then((x) => console.log(x))
-            .catch((x) => console.error(x))
-            .finally(() => console.log('finally'))
+        watch(
+            () => loggedIn,
+            async () => {
+                if (loggedIn.value === true) {
+                    await connDB()
+                }
+            }, {immediate: true}
+        )
     }
 
     return {
